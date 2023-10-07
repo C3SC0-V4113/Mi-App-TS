c513e3101099949c7148ebee152c468ec4bb7c1c
CESCO <cescovalledev@gmail.com>
2023-10-06 22:48:50 -0600

    useState in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Counter, Usuario } from "./components";
+
 function App() {
   return (
     <>
       <h1>Vite + React + TypeScript</h1>
+      <hr />
+      <Counter />
+      <Usuario />
     </>
   );
 }
